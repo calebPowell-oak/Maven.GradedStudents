e5492eb10d519ee4ddc13d5bd88694a1e04bbf90
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-05 17:18:00 -0400

    +getAverage and addStudent to Classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,44 @@
 package io.zipcoder;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 public class Classroom {
 
+    private Student[] students;
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom(int maxSize){
+        students = new Student[maxSize];
+    }
+
+    public Classroom() {
+        students = new Student[30];
+    }
+
+    //
+
+    public Student[] getStudents(){
+        return this.students;
+    }
+
+    public Double getAverageExamScore() {
+        Double compScore = 0d;
+        for(Student x : students){
+            compScore += x.getAverageExamScore();
+        }
+        return compScore / students.length;
+    }
+
+    public Boolean addStudent(Student student) {
+        for(int i = 0; i < students.length; i++){
+            if(students[i] == null){
+                students[i] = student;
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,61 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ClassroomTest {
+
+    @Test
+    public void x(){
+        // Given
+
+        // When
+
+        // Assert
+    }
+
+    @Test
+    public void getAverageExamScoreTest(){
+        // Given
+        Double[] s1Scores = {100.0, 150.0};
+        Double[] s2Scores = {225.0, 25.0};
+
+        Student s1 = new Student("S", "1", s1Scores);
+        Student s2 = new Student("S", "2", s2Scores);
+
+        Student[] students = new Student[] {s1, s2};
+        Classroom cr = new Classroom(students);
+
+        // When
+        Double actual = cr.getAverageExamScore();
+        Double expected = 125.0;
+
+        // Assert
+        Assert.assertEquals(expected, actual, 0.0001);
+    }
+
+    @Test
+    public void addStudentTest(){
+        // Given
+        Student[] students = {new Student(), new Student(), new Student(), null};
+        Classroom cr = new Classroom(students);
+
+        // When
+        Boolean successful = cr.addStudent(new Student());
+
+        // Assert
+        Assert.assertTrue(successful);
+    }
+
+    @Test
+    public void addStudentPastMaxTest(){
+        // Given
+        Classroom cr = new Classroom(0);
+
+        // When
+        Boolean successful = cr.addStudent(new Student());
+
+        // Assert
+        Assert.assertFalse(successful);
+    }
 }
