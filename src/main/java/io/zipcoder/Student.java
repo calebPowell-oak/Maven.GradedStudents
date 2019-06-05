9e65a3e15d5eb819203b3c45ade5b400a3b4412d
Caleb Powell <calebpowell@zipcoders-MacBook-Pro.local>
2019-06-05 16:39:52 -0400

    Implemented Student and StudentTest classes

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,89 @@
 package io.zipcoder;
 
+import org.omg.CORBA.DoubleHolder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examsScores = new ArrayList<>(0);
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examsScores.addAll(Arrays.asList(testScores));
+    }
+
+    public Student(){
+        this.firstName = "";
+        this.lastName = "";
+    }
+
+    public void addExamScore(Double newScore){
+        examsScores.add(newScore);
+    }
+
+    public Integer getTestsTaken(){
+        return examsScores.size();
+    }
+
+    public Boolean setExamScore(Integer testIndex, Double newScore){
+        if(testIndex >= 0 && testIndex <= examsScores.size()){
+            examsScores.set(testIndex, newScore);
+            return true;
+        }
+        return false;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getExamsScores() {
+        String output = "Exam Scores: \n";
+        Integer count = 1;
+        for(Double score : examsScores){
+            output += String.format("\tExam %d -> %.0f\n", count++, score);
+        }
+        return output;
+    }
+
+    public Double getAnExamScore(Integer testIndex){
+        return this.examsScores.get(testIndex);
+    }
+
+    public Double getAverageExamScore() {
+        Double count = 0d;
+        for(Double score : examsScores){
+            count += score;
+        }
+        return count /= examsScores.size();
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        output += String.format("Student Name: " + this.firstName + " " + this.lastName +
+                "\n> Average Score: %.0f\n" + "> Exam Scores: \n", getAverageExamScore());
+        Integer count = 1;
+        for(Double score : examsScores){
+            output += String.format("\tExam %d -> %.0f\n", count++, score);
+        }
+        return output;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,114 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    @Test
+    public void getScoresTest(){
+        // Given
+        Student caleb = new Student("Caleb", "Powell", new Double[] {100d,44d,77d});
+        String expected = "Exam Scores: \n" +
+                "\tExam 1 -> 100\n" +
+                "\tExam 2 -> 44\n" +
+                "\tExam 3 -> 77\n";
+
+        // When
+        String actual = caleb.getExamsScores();
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void x(){
+        // Given
+
+        // When
+
+        // Assert
+
+    }
+
+    @Test
+    public void addTestScoreTest(){
+        // Given
+        Student caleb = new Student();
+        Integer expected = 1;
+
+        // When
+        caleb.addExamScore(77d);
+        Integer actual = caleb.getTestsTaken();
+
+        // Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setTestScoreTest(){
+        // Given
+        Student caleb = new Student();
+        caleb.addExamScore(10d);
+
+        // When
+        Integer testIndex = 0;
+        Double expected = 100d;
+        Assert.assertTrue(caleb.setExamScore(testIndex, expected));
+
+        // Assert
+        Double actual = caleb.getAnExamScore(testIndex);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setTestScoreBadIndex(){
+        // Given
+        Student caleb = new Student();
+        caleb.addExamScore(10d);
+
+        // When
+        Integer testIndex = 10;
+        Double newScore = 100d;
+
+        // Assert
+        Assert.assertFalse(caleb.setExamScore(testIndex, newScore));
+    }
+
+    @Test
+    public void getAverageExamScore(){
+        // Given
+        Double scores[] = {100d, 100d, 100d};
+        Student caleb = new Student("C", "P", scores);
+        Double count = 0d;
+        for(Double x : scores){
+            count += x;
+        }
+
+        // When
+        Double expected = count /= scores.length;
+        Double actual = caleb.getAverageExamScore();
+
+        // Assert
+        Assert.assertEquals(expected, actual, 0.0001);
+    }
+
+    @Test
+    public void toStringTest(){
+        // Given
+        Double scores[] = {101d, 100d, 102d};
+        Student caleb = new Student("C", "P", scores);
+
+        // When
+        String expected = String.format("Student Name: " + caleb.getFirstName() + " " + caleb.getLastName() +
+                "\n> Average Score: %.0f\n" + "> Exam Scores: \n", caleb.getAverageExamScore()) +
+                "\tExam 1 -> 101\n" +
+                "\tExam 2 -> 100\n" +
+                "\tExam 3 -> 102\n";
+
+        // Assert
+        String actual = caleb.toString();
+        Assert.assertEquals(expected, actual);
+    }
 }
